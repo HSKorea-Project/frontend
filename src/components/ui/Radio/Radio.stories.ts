3c9c9bf6fa207c3d71a961032ed92689c9db2881
김민지 <alswlk0306@gmail.com>
2026-03-30 03:21:30 +0900

    design: Radio 컴포넌트 제작 및 스토리 작성

---
+++ src/components/ui/Radio/Radio.stories.ts
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from "@storybook/nextjs-vite";
+import Radio from "./Radio";
+
+const meta: Meta<typeof Radio> = {
+  title: "UI/RadioGroup",
+  component: Radio,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Radio>;
+
+// 선택
+export const Checked = {
+  args: {
+    label: "포함",
+    checked: true,
+  },
+};
+
+// 미선택
+export const Unchecked = {
+  args: {
+    label: "포함",
+    checked: false,
+  },
+};
---
+++ src/components/ui/Radio/Radio.tsx
@@ -0,0 +1,59 @@
+import { css } from "@/styled-system/css";
+
+// Radio Props 타입 정의
+interface RadioProps {
+  checked: boolean;
+  label: string;
+  onClick: () => void;
+}
+
+// Radio 컴포넌트
+export default function Radio({ checked, label, onClick }: RadioProps) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={css({
+        display: "flex",
+        alignItems: "center",
+        gap: "6px",
+        cursor: "pointer",
+      })}
+    >
+      {/* 원 */}
+      <div
+        className={css({
+          width: "16px",
+          height: "16px",
+          borderRadius: "full",
+          border: "1px solid",
+          borderColor: checked ? "green.500" : "black",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        })}
+      >
+        {checked && (
+          <div
+            className={css({
+              width: "10px",
+              height: "10px",
+              borderRadius: "full",
+              bg: "green.500",
+            })}
+          />
+        )}
+      </div>
+
+      {/* 텍스트 */}
+      <span
+        className={css({
+          fontSize: "16px",
+          color: "gray.700",
+        })}
+      >
+        {label}
+      </span>
+    </button>
+  );
+}
