2b7f749ce42f91bde50f09e57667f94f33e6e25c
김민지 <alswlk0306@gmail.com>
2026-04-21 17:17:24 +0900

    refactor: 변경된 파일명으로 경로 수정

--- src/app/(main)/inquiry/new/_components/Customer.tsx
+++ src/app/(main)/inquiry/new/_components/Customer.tsx
@@ -7,16 +7,7 @@ import Dropdown from "@/components/ui/Dropdown/Dropdown";
 import Button from "@/components/ui/Button/Button";
 import { formatPhoneNumber, onlyNumber } from "@/utils/format";
 import { useEffect, useRef, useState } from "react";
-
-// 통신사
-export const AGENCY = [
-  { label: "KT", value: "kt" },
-  { label: "SKT", value: "skt" },
-  { label: "LG U+", value: "lg" },
-  { label: "알뜰폰 KT", value: "al_kt" },
-  { label: "알뜰폰 SKT", value: "al_skt" },
-  { label: "알뜰폰 LG U+", value: "al_lg" },
-];
+import { AGENCY } from "@/constants/agency";
 
 // Customer Props 타입 정의
 interface CustomerProps {
