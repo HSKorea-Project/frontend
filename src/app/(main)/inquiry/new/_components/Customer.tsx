37f8133330311275873592dbafb9202ff34ccfab
김민지 <alswlk0306@gmail.com>
2026-04-21 17:13:55 +0900

    rename: 상수 파일명 대소문자 변경

--- src/app/(main)/inquiry/new/_components/Customer.tsx
+++ src/app/(main)/inquiry/new/_components/Customer.tsx
@@ -6,8 +6,17 @@ import Input from "@/components/ui/Input/Input";
 import Dropdown from "@/components/ui/Dropdown/Dropdown";
 import Button from "@/components/ui/Button/Button";
 import { formatPhoneNumber, onlyNumber } from "@/utils/format";
-import { AGENCY } from "@/constants/Agency";
 import { useEffect, useRef, useState } from "react";
+
+// 통신사
+export const AGENCY = [
+  { label: "KT", value: "kt" },
+  { label: "SKT", value: "skt" },
+  { label: "LG U+", value: "lg" },
+  { label: "알뜰폰 KT", value: "al_kt" },
+  { label: "알뜰폰 SKT", value: "al_skt" },
+  { label: "알뜰폰 LG U+", value: "al_lg" },
+];
 
 // Customer Props 타입 정의
 interface CustomerProps {
---
+++ src/constants/moveType.ts
@@ -0,0 +1,11 @@
+// 이사 유형 드롭다운
+export const MOVE_TYPE = [
+  { label: "사무실/관공서 이사", value: "office_move" },
+  { label: "학교이사", value: "school_move" },
+  { label: "물류창고", value: "warehouse_move" },
+  { label: "병원이사", value: "hospital_move" },
+  { label: "실험실이사", value: "lab_move" },
+  { label: "공장이사", value: "factory_move" },
+  { label: "상업시설이사", value: "commercial_move" },
+  { label: "가정이사", value: "home_move" },
+];
