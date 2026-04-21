b2905b07650d087bb6eb94496dd52d6a9dd04447
김민지 <alswlk0306@gmail.com>
2026-04-21 17:03:40 +0900

    refactor: 파일명 변경 시 대소문자 인식 문제로 인해 파일 삭제

--- src/constants/Agency.ts
+++
@@ -1,9 +0,0 @@
-// 통신사
-export const AGENCY = [
-  { label: "KT", value: "kt" },
-  { label: "SKT", value: "skt" },
-  { label: "LG U+", value: "lg" },
-  { label: "알뜰폰 KT", value: "al_kt" },
-  { label: "알뜰폰 SKT", value: "al_skt" },
-  { label: "알뜰폰 LG U+", value: "al_lg" },
-];
