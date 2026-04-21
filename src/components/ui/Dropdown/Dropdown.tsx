072f1150bbac6be2048508b25f0cb36982121cdb
김민지 <alswlk0306@gmail.com>
2026-04-22 01:34:53 +0900

    style: 버튼 타입 추가

--- src/components/ui/Dropdown/Dropdown.tsx
+++ src/components/ui/Dropdown/Dropdown.tsx
@@ -89,6 +89,7 @@ export default function Dropdown({
 
       {/* 버튼 */}
       <button
+        type="button"
         onClick={() => setOpen((prev) => !prev)}
         className={css({
           display: "flex",
