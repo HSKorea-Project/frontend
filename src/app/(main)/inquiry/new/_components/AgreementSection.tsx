e8f8826a5ef475ba66e089d36fdba264e015ed88
김민지 <alswlk0306@gmail.com>
2026-04-21 16:24:18 +0900

    style: 필요없는 주석 삭제

--- src/app/(main)/inquiry/new/_components/AgreementSection.tsx
+++ src/app/(main)/inquiry/new/_components/AgreementSection.tsx
@@ -67,18 +67,6 @@ export default function AgreementSection({ privacy, setPrivacy }: AgreementProps
         })}
       />
 
-      {/* 안내 문구 */}
-      {/* <p
-        className={css({
-          fontWeight: "regular",
-          color: "gray.500",
-          fontSize: { base: "12px" },
-          textAlign: "center",
-        })}
-      >
-        문의 내용은 담당자 검토 후 빠른 시일 내 연락드립니다.
-      </p> */}
-
       {/* 버튼 */}
       <Button variant="primary" fullWidth className={css({ maxWidth: "200px", marginX: "auto" })}>
         문의하기
