03c0732980586560c548a196fc71b55a99b38023
김민지 <alswlk0306@gmail.com>
2026-04-23 21:36:53 +0900

    refactor: 확장성을 위한 리팩토링

--- src/components/ui/SectionTitle/SectionTitle.tsx
+++ src/components/ui/SectionTitle/SectionTitle.tsx
@@ -1,15 +1,16 @@
-import { css } from "@/styled-system/css";
+import { css, cx } from "@/styled-system/css";
 import { ReactNode } from "react";
 
 // Section 제목 Props 타입 정의
 interface SectionTitleProps {
   children: ReactNode;
   enTitle?: string;
-  align: "left" | "center";
+  align?: "left" | "center";
   gap?: number;
   barWidth?: number;
   showBar?: boolean;
-  color?: "white" | "gray.900";
+  color?: string;
+  className?: string;
 }
 
 // Section 제목 컴포넌트
@@ -21,47 +22,60 @@ export default function SectionTitle({
   barWidth = 30,
   showBar = true,
   color = "gray.900",
+  className,
 }: SectionTitleProps) {
+  const isCenter = align === "center";
+
   return (
     <div
-      className={css({
-        display: "flex",
-        flexDirection: "column",
-        rowGap: `${gap}px`,
-        alignItems: align,
-        width: "fit",
-      })}
+      className={cx(
+        css({
+          display: "flex",
+          flexDirection: "column",
+          alignItems: isCenter ? "center" : "flex-start",
+          rowGap: `${gap}px`,
+          width: "fit-content",
+        }),
+        className
+      )}
     >
+      {/* 영어 타이틀 */}
       {enTitle && (
         <p
           className={css({
             fontWeight: "bold",
             fontSize: "12px",
             color: "orange.400",
+            textAlign: isCenter ? "center" : "left",
           })}
         >
           {enTitle}
         </p>
       )}
+
+      {/* 메인 타이틀 */}
       <h2
         className={css({
-          fontSize: "24px",
+          fontSize: { base: "20px", md: "24px" },
           fontWeight: "bold",
-          color: color,
-          textAlign: align,
+          color,
+          textAlign: isCenter ? "center" : "left",
         })}
       >
         {children}
       </h2>
-      <div
-        className={css({
-          display: showBar ? "block" : "none",
-          width: `${barWidth}px`,
-          height: "3px",
-          borderRadius: "full",
-          bg: "orange.400",
-        })}
-      />
+
+      {/* 바 */}
+      {showBar && (
+        <div
+          className={css({
+            width: `${barWidth}px`,
+            height: "3px",
+            borderRadius: "full",
+            bg: "orange.400",
+          })}
+        />
+      )}
     </div>
   );
 }
