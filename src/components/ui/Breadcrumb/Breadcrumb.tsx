353fce85f6cade44eaeb96876fcf62ef3ea3d5c7
김민지 <alswlk0306@gmail.com>
2026-04-24 01:19:49 +0900

    design: breadcrumb 컴포넌트 생성

---
+++ src/components/ui/Breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,34 @@
+import RightWingIcon from "@/assets/svgs/right-wing.svg";
+import { css } from "@/styled-system/css";
+
+// Breadcrumb Props 타입 정의
+interface BreadcrumbProps {
+  currentPageName: string;
+}
+
+// Breadcrumb 컴포넌트
+export default function Breadcrumb({ currentPageName }: BreadcrumbProps) {
+  return (
+    <div
+      className={css({
+        display: "flex",
+      })}
+    >
+      <p
+        className={css({
+          color: "white",
+        })}
+      >
+        홈
+      </p>
+      <RightWingIcon width={24} height={24} fill="#fff" />
+      <p
+        className={css({
+          color: "orange.400",
+        })}
+      >
+        {currentPageName}
+      </p>
+    </div>
+  );
+}
