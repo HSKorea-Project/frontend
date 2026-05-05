675ef34e5955d690ca2140a0be73bb33191a0085
김민지 <alswlk0306@gmail.com>
2026-05-05 16:07:45 +0900

    feat: 메타 데이터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,14 @@
 import ModalProvider from "@/components/modal/ModalProvider";
 import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "에이치에스코리아",
+  description: "에이치에스코리아 이사 견적 문의",
+  icons: {
+    icon: "./favicon.ico",
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
